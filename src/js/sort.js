c63ce91127ed17e60f7e04fa04194a7875e89994
Alexander B <bas2784@gmail.com>
2023-03-31 15:20:28 +0300

    add logic

---
+++ src/js/sort.js
@@ -0,0 +1,26 @@
+function sortByKey(arr) {
+	arr.sort((a, b) => (a > b ? 1 : -1));
+}
+
+export default function orderByProps(param1, param2) {
+	const ar = [];
+	const tmp = [];
+	for (const prop in param1) {
+		if (param2.includes(prop)) {
+			ar.push(
+				{ key: prop, value: param1[prop] },
+			);
+		} else {
+			tmp.push(prop);
+		}
+	}
+
+	sortByKey(tmp);
+
+	for (let i = 0; i < tmp.length; i += 1) {
+		ar.push(
+			{ key: tmp[i], value: param1[tmp[i]] },
+		);
+	}
+	return ar;
+}
