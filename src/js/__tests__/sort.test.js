2ecec68cc4d9efa0ec394c73a9e2ea8968e8954d
Alexander B <bas2784@gmail.com>
2023-03-31 16:42:05 +0300

    lint fix

--- src/js/__tests__/sort.test.js
+++ src/js/__tests__/sort.test.js
@@ -1,21 +1,21 @@
 import orderByProps from '../sort';
 
 const obj = {
-	name: 'мечник',
-	health: 10,
-	level: 2,
-	attack: 80,
-	defence: 40,
+  name: 'мечник',
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
 };
 
 const expected = [
-	{ key: 'name', value: 'мечник' },
-	{ key: 'attack', value: 80 },
-	{ key: 'defence', value: 40 },
-	{ key: 'health', value: 10 },
-	{ key: 'level', value: 2 },
+  { key: 'name', value: 'мечник' },
+  { key: 'attack', value: 80 },
+  { key: 'defence', value: 40 },
+  { key: 'health', value: 10 },
+  { key: 'level', value: 2 },
 ];
 
 test('тестируем результат сортировки свойств объекта', () => {
-	expect(orderByProps(obj, ['name'])).toEqual(expected);
+  expect(orderByProps(obj, ['name'])).toEqual(expected);
 });
